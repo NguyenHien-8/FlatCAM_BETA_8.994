ac8b8506907919e9ab49ff802d478de9c8ef26fa
NGUYENHIEN <trannguyenhien29085@gmail.com>
2025-09-10 13:57:31 +0700

    change README and Config

--- FlatCAM_Config.bat
+++ FlatCAM_Config.bat
@@ -1,7 +1,10 @@
 @echo off
 echo ====== Install Library FlatCAM 8.994 (Windows) ======
+echo Installing Python 3.9 Virtual Environment...
 py -3.9 -m venv env39
+echo Installed Successfully
 call env39\Scripts\activate
+echo Virtual Environment Activation Successful
 python -m pip install --upgrade pip
 python -m pip install GDAL-3.4.3-cp39-cp39-win_amd64.whl
 python -m pip install -r requirements.txt
